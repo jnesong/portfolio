32c4b1ab92d72259933e016ad822036dba744884
Jenny Chau <jnechau@gmail.com>
2022-03-05 20:13:17 -0500

    enneadrink calendar function done

--- client/src/components/enneadrink/user/journal/History.js
+++ client/src/components/enneadrink/user/journal/History.js
@@ -1,12 +1,18 @@
 //libraries
 import { useState, useEffect } from 'react';
+import Calendar from 'react-calendar';
 //components
 import PastEntry from './PastEntry';
+//styling
+// import 'react-calendar/dist/Calendar.css';
+
 
 function History() {
 
     const [journalHistory, setJournalHistory] = useState([]);
-
+    const [calDate, setCalDate] = useState(new Date())
+    const [filteredHistory, setFilteredHistory] = useState([])
+    const [monthTitle, setMonthTitle] = useState("")
 
     useEffect(() => {
         fetch("/api/journeys")
@@ -17,6 +23,7 @@ function History() {
     function holdDeletedID(id) {
         const updatedJournalHistory = journalHistory.filter(journey => journey.id !== id)
         setJournalHistory(updatedJournalHistory)
+        setFilteredHistory(updatedJournalHistory)
     }
 
     function holdEdit(editedJourney) {
@@ -29,9 +36,28 @@ function History() {
             }
         })
         setJournalHistory(updatedJournalHistory)
+        setFilteredHistory(updatedJournalHistory)
     }
 
-    const listPastEntries = journalHistory.map(journey => (
+    function onChangeDate(calDate) {
+        setCalDate(calDate)
+    }
+
+    useEffect(() => {
+        searchHistory(calDate)
+        monthDisplay(calDate)
+    }, [calDate])
+
+    function searchHistory(date) {
+        const monthMap = { "0": "01", "1": "02", "2": "03", "3": "04", "4": "05", "5": "06", "6": "07", "7": "08", "8": "09", "9": "10", "10": "11", "11": "12" }
+        let calMonth = monthMap[date.getMonth()]
+        let calYearString = String(date).split(" ")
+        let calYear = calYearString[3]
+        let filteredJournal = journalHistory.filter( journey => journey.date.slice(5, 7)===calMonth && journey.date.slice(0,4)===calYear )
+        setFilteredHistory(filteredJournal)
+    }
+
+    const listPastEntries = filteredHistory.map(journey => (
         <PastEntry
             key={journey.id}
             journey={journey}
@@ -40,11 +66,25 @@ function History() {
         />
     ))
 
-    return (
-        <>
-            {listPastEntries}
+    function monthDisplay(date) {
+        const monthDisplayMap = { "0": "January", "1": "February", "2": "March", "3": "April", "4": "May", "5": "June", "6": "July", "7": "August", "8": "September", "9": "October", "10": "November", "11": "December" }
+        let month = monthDisplayMap[date.getMonth()]
+        setMonthTitle(month)
+    }
 
-        </>
+    return (
+        <div className="history-row">
+
+            <div className="history-column-left">
+                <div id="history-calendar">
+                    <Calendar onChange={onChangeDate} value={calDate} />
+                </div>
+            </div>
+            <div className="history-column-right">
+                <p className="month-title"> {monthTitle} </p>
+                {listPastEntries}
+            </div>
+        </div>
     )
 }
 
